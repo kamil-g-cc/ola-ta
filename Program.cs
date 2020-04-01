71c730457906a5aa1f84c07ac8f2780804dc4ad5
Kamil Grondys <k.grondys@samsung.com>
2020-04-01 16:01:29 +0200

    adding interface and word iterator
    Adding IIterator and WordIterator objects

--- Program.cs
+++ Program.cs
@@ -2,11 +2,56 @@
 
 namespace ola_ta
 {
+    public class FileContent
+    {
+
+    }
+    interface IIterator
+    {
+        /*
+            return either there is an another element on the list
+        */
+        public bool HasNext();
+        /*
+            returns the current element on the list and move to the next element
+        */
+        public string MoveNext();
+        /*
+            currently does nothing
+        */
+        public void Remove();
+    }
+
+    class WordIterator : IIterator
+    {
+        public bool HasNext()
+        {
+            //TODO: implement
+            return false;
+        }
+        public string MoveNext()
+        {
+            //TODO: implment
+            return "";
+        }
+        public void Remove()
+        {
+
+        }
+        public WordIterator(FileContent fileContent)
+        {
+
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //var file_content = new FileContent();
+            var moj_pierwszy_iterator = new WordIterator(new FileContent());
+            Console.WriteLine("Bye, bye!");
         }
     }
 }
