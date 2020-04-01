2840d5b05123912ec656dd98b438bcf6afbee953
Kamil Grondys <k.grondys@samsung.com>
2020-04-01 15:48:28 +0200

    init repo
    Adding Program.cs and project configuration file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ola_ta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
